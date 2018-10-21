48d6246cf034482668ca4ca0d2d52ec2c1df27f1
Amy Pham <amp4129@g.rit.edu>
2018-10-21 11:16:55 -0700

    updates, fix scroll

--- scripts.js
+++ scripts.js
@@ -7,3 +7,40 @@ function selectPart(){
 function getSelectedParts(){
 	
 }
+
+function bindScroll(){
+		console.log('bound');
+	//$(window).scroll(function (e) {
+	    pos = 365;
+	    if ($(window).scrollTop() > pos) {
+	        $('#activeContent').css({
+	            position: 'relative',
+	            top: 0,
+	            bottom: 'auto'
+	        });
+	    } else {
+	        $('#activeContent').css({
+	            position: 'fixed',
+	            top: 'auto',
+	            bottom: 0
+	        });
+	    }
+	//});
+}
+
+function mobileThings(){
+  if(window.innerHeight > window.innerWidth){
+    $('.arrow').attr('src', 'assets/arrowM.svg');
+    $(window).on('scroll', bindScroll);
+  }
+  else{
+	console.log('unbind');
+    $(window).off('scroll');
+    
+    $('#activeContent').css({
+        position: 'relative',
+        top: 0,
+        bottom: 'auto'
+    });
+  }
+}
