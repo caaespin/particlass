bb8f817ef73641a0cdce111953ff62f584d84188
sauloho <sauloho@gmail.com>
2018-10-21 12:04:07 -0700

    Updated website

--- static/scripts.js
+++ static/scripts.js
@@ -1,9 +1,47 @@
 function selectPart(){
 	$('td').click(function(){
-		$(this).addClass('selected');
+		$(this).toggleClass('selected');
+		$('#numSamples').html($('.selected').length-1);
 	});
 }
 
 function getSelectedParts(){
 	
+}
+
+function bindScroll(){
+		console.log('bound');
+	//$(window).scroll(function (e) {
+	    pos = 365;
+	    if ($(window).scrollTop() > pos) {
+	        $('#activeContent').css({
+	            position: 'relative',
+	            top: 0,
+	            bottom: 'auto'
+	        });
+	    } else {
+	        $('#activeContent').css({
+	            position: 'fixed',
+	            top: 'auto',
+	            bottom: 0
+	        });
+	    }
+	//});
+}
+
+function mobileThings(){
+  if(window.innerHeight > window.innerWidth){
+    $('.arrow').attr('src', 'assets/arrowM.svg');
+    $(window).on('scroll', bindScroll);
+  }
+  else{
+	console.log('unbind');
+    $(window).off('scroll');
+
+    $('#activeContent').css({
+        position: 'relative',
+        top: 0,
+        bottom: 'auto'
+    });
+  }
 }
