08976f1fe616d31585c5d5b89cf909455bcf82fd
Amy Pham <amp4129@g.rit.edu>
2018-10-21 12:34:06 -0700

    adding submit function

--- scripts.js
+++ scripts.js
@@ -1,12 +1,40 @@
 function selectPart(){
 	$('td').click(function(){
 		$(this).toggleClass('selected');
-		$('#numSamples').html($('.selected').length-1);
+		$('#numSamples').html($('.selected').length);
 	});
 }
 
 function getSelectedParts(){
-	
+	var selectedNames = [];
+	var selectedImages = $('.selected').children('img').each(function(){
+		selectedNames.push($(this).attr('id'));
+	});
+
+	var names = [];
+	var images = $('td').children('img').each(function(){
+		names.push($(this).attr('id'));
+	});
+
+	console.dir(names);
+	console.dir(selectedNames);
+	var post = {};
+	for(var i = 0; i < names.length; i++){
+		if(selectedNames.includes(names[i])){
+			post[names[i]] = true;
+		}
+		else{
+			post[names[i]] = false;
+		}
+	}
+
+	console.dir(post);
+	$.ajax({
+	  type: "POST",
+	  url: 'http://af423aef.ngrok.io/classify',
+	  data: post,
+	  dataType: 'JSON'
+	});
 }
 
 function bindScroll(){
@@ -45,3 +73,5 @@ function mobileThings(){
     });
   }
 }
+
+
